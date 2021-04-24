ede75365fb6c3065243e6649a52725bc7ced2816
Shahid Akhtar <shahidakhtar016@gmail.com>
2021-04-24 16:40:27 +0530

    Updated graph data

--- dist/js/userInfo.js
+++ dist/js/userInfo.js
@@ -4,7 +4,7 @@ html_score = sessionStorage.getItem("html_score");
 css_score = sessionStorage.getItem("css_score");
 js_score = sessionStorage.getItem("js_score");
 node_score = sessionStorage.getItem("node_score");
-mongo_score = sessionStorage.getItem("mongo_score");
+php_score = sessionStorage.getItem("php_score");
 // document.querySelector("span.name").innerHTML = user_name;
 // document.querySelector("span.points").innerHTML = user_points;
 
